2015462590ee9ec7f478175b042e1d151290c5c4
Kirsten0604 <hungrytacocat@gmail.com>
2018-03-13 13:52:06 -1000

    functions on start page

---
+++ startEvent.js
@@ -0,0 +1,13 @@
+//click real button to get redirected to real1.html
+$(document).ready(function(){
+    $("#real").click(function(){
+        location.href = "real1.html";
+    });
+});
+
+//click drill button to get redirected to drill1.html
+$(document).ready(function(){
+    $("#drill").click(function(){
+        location.href = "drill1.html";
+    });
+});
