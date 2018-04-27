aae15c1fbd230efb49e5855e7c181044da6b5849
alicewy <alicewy@hawaii.edu>
2018-04-27 05:00:29 -1000

    testing email messaging apis

--- sendText.js
+++ sendText.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $("#yousure").click(function(){
         location.href = "sentToSupervisor.html";
-        //execute cmd line command "node msg.js"
 
     });
 });
